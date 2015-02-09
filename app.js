c93b318bf22d369c60b788e81c46f5cbd025a4f9
Sandya Sankarram <ss4119@columbia.edu>
2015-02-09 18:24:00 -0500

    almost done

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ var fs = require('fs');
 var express = require('express');
 var logger = require('morgan');
 var async = require('async');
+var ejs = require('ejs')
+
 
 
 
 // take a list of files from the command line.
 // now we can watch three files using:
 // node app.js file1.js file2.js file3.js
-var filenames = Array.prototype.slice.call(process.argv);
-console.log(filenames[2]);
+var filenames = Array.prototype.slice.call(process.argv, 2);
 
 // create the express app
 var app = express();
@@ -17,31 +18,34 @@ var app = express();
 // connect the Morgan logging middleware to our app
 app.use(logger('dev'));
 
+
+app.set('view engine', 'ejs');
+
+
 // start a server listening on port 1234
-app.listen(6101);
+app.listen(8111);
 
 
 app.get('/', function (request, response) {
-  var fsreadFile = function(result, callback) {
-       fs.readFile(result, function(err, data) {
-        if (data !== "undefined"){
-            callback(data);
-        }
+
+
+  var mapFilenamesToContent = function(filename, callback) {
+       fs.readFile(filename, function(err, data) {
+            return callback(null, data.toString());
         });
-
-        };
- 
-
-  var mapFilenamesToContent = function(filename) {
-    async.map(filenames, fsreadFile, function(err, results){console.log(results);});
   };
 
   async.map(filenames, mapFilenamesToContent, function (err, results) {
-    if (err) console.log('async.map error:', err);
+    if (err) console.log('async.map error:', err);  
     response.send( '<pre>' + results.join("\n\n") + '</pre>' );
+
   });
+
+
+
 });
 
+// PART 1 
 // when someone requests http://localhost:1234/, run the callback
 // function listed here and respond with the data
 // we call this the "/" (or "Root") route.
@@ -51,7 +55,7 @@ app.get("/", function(request, response) {
     response.writeHead(200, {
         "Content-Type": "text/css"
     });
-    for (var i = 2; i < filenames.length; i++) {
+    for (var i = 0; i < filenames.length; i++) {
         var data_str = "";
 
 
@@ -60,7 +64,7 @@ app.get("/", function(request, response) {
         });
 
     }
-    if (i == filenames.length - 1) {
+    if (i == filenames.length - 3) {
         response.end();
     }
 
