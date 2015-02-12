77cc414f17dbec2c2baf883e7f5141a188fbf839
Sandya Sankarram <ss4119@columbia.edu>
2015-02-12 10:14:36 -0500

    init

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var ejs = require('ejs')
 // now we can watch three files using:
 // node app.js file1.js file2.js file3.js
 var filenames = Array.prototype.slice.call(process.argv, 2);
+//var socketio = require('socket.io');
+
 
 // create the express app
 var app = express();
@@ -23,27 +25,40 @@ app.set('view engine', 'ejs');
 
 
 // start a server listening on port 1234
-app.listen(8111);
+var server = app.listen(6543);
+//var io = socketio.listen( server );
+
+
 
 
 app.get('/', function (request, response) {
 
 
   var mapFilenamesToContent = function(filename, callback) {
-       fs.readFile(filename, function(err, data) {
-            return callback(null, data.toString());
-        });
+       //fs.readFile(filename, function(err, data) {
+       //     return callback(null, data.toString());
+       // });
+
+        fs.readFile(filename, 'utf8', function(err, data){
+              if (data== undefined) data= "";
+              callback(null,{ id: filename.replace(/[^0-9]/ig, ""),
+              data: data.toString(),
+              filename: filename
+          })
+      });    
   };
 
   async.map(filenames, mapFilenamesToContent, function (err, results) {
     if (err) console.log('async.map error:', err);  
-    response.send( '<pre>' + results.join("\n\n") + '</pre>' );
-
+    //response.send( '<pre>' + results.join("\n\n") + '</pre>' );
+    response.render( 'mainView', { files: results} );
   });
 
 
 
 });
+
+
 
 // PART 1 
 // when someone requests http://localhost:1234/, run the callback
